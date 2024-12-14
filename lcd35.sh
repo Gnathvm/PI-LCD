d705e1c05531b932aac1ed48699a065eba82e577
Gokul Nath VM <gnathvm@gmail.com>
2024-12-14 11:32:58 +0530

    upstream

--- lcd35.sh
+++ lcd35.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -d /etc/X11/xorg.conf.d ]; then
-sudo mkdir -p /etc/X11/xorg.conf.d
-fi
-sudo cp ./usr/lcd35a-overlay.dtb /boot/overlays/lcd35a.dtbo
-sudo cp ./usr/lcd35a-overlay.dtb /boot/overlays/
+sudo apt-get install cmake libraspberrypi-dev -y
+sudo apt-get install xserver-xorg-input-evdev -y
+sudo apt-get install xserver-xorg-input-evdev xinput-calibrator -y
+
+sudo cp ./usr/lcd35a.dtb /boot/overlays/lcd35a.dtbo
 
 sudo cp /boot/firmware/config.txt ./usr/config.txt
 sudo echo "hdmi_force_hotplug=1" >> ./usr/config.txt
@@ -17,19 +17,20 @@ sudo echo "hdmi_mode=1" >> ./usr/config.txt
 sudo echo "hdmi_mode=87" >> ./usr/config.txt
 sudo echo "hdmi_cvt 480 320 60 6 0 0 0" >> ./usr/config.txt
 sudo echo "hdmi_drive=2" >> ./usr/config.txt
-
 sudo cp -rf ./usr/config.txt /boot/firmware/config.txt
 
+if [ -f /etc/X11/xorg.conf.d/40-libinput.conf ]; then
+sudo rm -rf /etc/X11/xorg.conf.d/40-libinput.conf
+fi
+if [ ! -d /etc/X11/xorg.conf.d ]; then
+sudo mkdir -p /etc/X11/xorg.conf.d
+fi
 
-#sudo cp -rf ./usr/99-calibration.conf-35-90  /etc/X11/xorg.conf.d/99-calibration.conf
-#sudo cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
+sudo cp -rf ./etc/rc.local /etc/
+sudo cp -rf ./usr/99-calibration35.conf  /etc/X11/xorg.conf.d/99-calibration.conf
+sudo cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
 
-#sudo cp ./usr/inittab /etc/
-#sudo cp -rf ./etc/rc.local /etc/
-
-sudo apt-get install cmake libraspberrypi-dev -y
-sudo apt-get install xserver-xorg-input-evdev -y
-sudo apt-get install xserver-xorg-input-evdev xinput-calibrator -y
+sudo cp ./usr/inittab /etc/
 
 sudo mkdir ./rpi-fbcp/build
 cd ./rpi-fbcp/build/
@@ -38,8 +39,8 @@ sudo make
 sudo install fbcp /usr/local/bin/fbcp
 cd - > /dev/null
 
+sudo raspi-config nonint do_wayland W1
 
-sudo sync
 sudo sync
 sleep 1
 
