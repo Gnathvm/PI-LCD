c84652ecfc0fcd2badea358338d415e5fcf0859a
Gokul Nath VM <gnathvm@gmail.com>
2024-12-17 13:45:04 +0530

    Update klcd35.sh

--- klcd35.sh
+++ klcd35.sh
@@ -29,8 +29,8 @@ fi
 
 sudo cp -rf ./etc/rc.local /etc/
 sudo cp -rf ./usr/99-calibration35.conf  /etc/X11/xorg.conf.d/99-calibration.conf
+sudo cp -rf ./usr/cmdline.txt /boot/
 sudo cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
-
 sudo cp ./usr/inittab /etc/
 
 sudo mkdir ./rpi-fbcp/build
